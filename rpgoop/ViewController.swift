b4bb07a990f584fc2dc38a125a840cb980dc8a5d
chonnawee4 <chonnawee@digio.co.th>
2016-04-07 14:23:51 +0700

    Initial Commit

---
+++ rpgoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpgoop
+//
+//  Created by Chonnawee on 4/7/2559 BE.
+//  Copyright © 2559 Chonnawee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
